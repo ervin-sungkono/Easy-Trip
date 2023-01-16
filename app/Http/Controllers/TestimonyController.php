e03b59e845693d16b53a466b7a25c70b42a307ca
rahman021102 <fathuraji99@gmail.com>
2023-01-16 23:34:15 +0700

    i created a CRUD logic in Testimony controler for the display of reviews

--- app/Http/Controllers/TestimonyController.php
+++ app/Http/Controllers/TestimonyController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class TestimonyController extends Controller
 {
+    public function Testimony(){
+        $testimony = Testimony::all();
+        return view('Testimony', ['Testimony'=>$testimony]);
+    }
 
+    public function createTestimony(Request $req){
+        $testimony = new Testimony();
+        $testimony->text = $req->text;
+        $testimony->rating = $req->rating;
+
+        $testimony-> save();
+    }
+
+    public function updateTestimony(Request $req){
+        $testimony = Testimony::find($req->id);
+
+        $testimony->text = $req->text != null ? $req->text:Testimony->text;
+        $testimony->rating = $req->rating != null ? $req->rating:Testimony->text;
+
+        $testimony-> save();
+    }
+    public function deleteTestimony($id){
+        $testimony = Testimony::find($id);
+        $testimony->delete();
+    }
 }
