4bb070d6b4fab1c4a5af62cd2ff7a4ac3de0de8c
kennathanael <knathanael3@gmail.com>
2023-01-16 22:25:31 +0700

    betulin pdf

--- resources/views/ticket/ticket.blade.php
+++ resources/views/ticket/ticket.blade.php
@@ -5,25 +5,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ticket</title>
+    <style>
+        body{
+            font-family: sans-serif;
+        }
+
+        .container{
+            padding: 16px;
+
+        }
+
+        .top{
+            display: flex;
+            flex-direction: column;
+        }
+
+        .tiket-id{
+            font-family: serif;
+        }
+
+        .nama{
+            color: grey;
+        }
+
+        .mid{
+            border-style: solid;
+            border-radius: 6px;
+            padding: 8px;
+            border-width: 1.5px;
+        }
+
+        .tanggal{
+            padding-top: 24px;
+        }
+
+        .ending{
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            width: 100%;
+            padding-top: 40px;
+        }
+    </style>
 </head>
 <body>
-
     <div class="container">
-        <div class="d-flex flex-column">
-            <h1>{{$ticket->ticket_id}}</h1>
-            <h3>{{$user->name}}</h3>
+        <div class="top">
+            <h1 class="tiket-id">ID Tiket: {{$ticket->ticket_id}}</h1>
+            <h3 class="nama">Nama: {{$user->name}}</h3>
         </div>
-    </div>
-    <div>
-        Item: {{$item->id}}
-        <br>
-        Item Name: {{$item->name}}
-        <br>
-        Quantity: {{$ticket->quantity}}
-
-    </div>
-    <div>
-        {{$ticket->ticket_date}}
+        <div class="mid">
+            <div class="konten" style="display: flex; flex-direction: column; gap: 8px;">
+                <div><b>Nama Item: </b> {{$item->name}}</div>
+                <div><b>Jumlah: </b> {{$ticket->quantity}}</div>
+                <div><b>Status: </b> {{$ticket->status? "Aktif":"Sudah Digunakan"}}</div>
+            </div>
+            <div class="tanggal">
+               <b>Tanggal: </b><i>{{$ticket->ticket_date}}</i>
+            </div>
+        </div>
+        <h2 class="ending">
+            <i>Have a nice trip :)</i>
+        </h2>
     </div>
 </body>
 </html>
