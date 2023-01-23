acc0db90d70210281b6608980b4207d667e5bb4b
Ervin Cahyadinata Sungkono <43065067+ervin-sungkono@users.noreply.github.com>
2023-01-23 16:39:45 +0700

    change description column type from string to text

--- database/migrations/2022_12_08_042619_create_items_table.php
+++ database/migrations/2022_12_08_042619_create_items_table.php
@@ -16,7 +16,7 @@ class CreateItemsTable extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->string('image');
             $table->string('location');
             $table->boolean('status')->default(0);
